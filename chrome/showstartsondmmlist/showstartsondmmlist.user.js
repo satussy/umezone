544ec1baa186861de0a11b212cb7b322a106b3b9
umezo <umezo100+github@gmail.com>
2010-06-19 19:56:51 +0900

    cleanup

--- chrome/showstartsondmmlist/showstartsondmmlist.user.js
+++ chrome/showstartsondmmlist/showstartsondmmlist.user.js
@@ -3,30 +3,94 @@
 // @namespace      umezo.tsuyabu.in
 // @include        http://www.dmm.co.jp/digital/videoa/-/list/=/article=series/id=9874/
 // ==/UserScript==
-
     if( location.href.indexOf("list") < 0 ){ return ; }
     var GET = "GET" ;
 	var doc = document ;
     var imgRegExp = new RegExp( "<img(\\s|.)*?([^<]*)>" , "ig")
+    var PlainHTMLUtil = (function(){
+        function getTags(html, tagName, id){
+            var cls = "";
+            if(id){
+                cls = "[^>]*?id=\"" + id + "\"";
+            }
+            var reg = new RegExp("<" + tagName + cls + "(\\s|.)*?>([^<]*)</" + tagName + ">", "ig");
+            return html.match(reg);
+        }
 
-    doc.addEventListener('DOMNodeInserted', handleInserted, false);
-    setStarts( $( ".text > a" ) ) ;
+        function getTag(html, tagName, className){
+            var tags = getTags(html, tagName, className);
+            return (tags && tags.length) ? tags[0] : "";
+        }
+
+        return {
+            getTags : getTags ,
+            getTag  : getTag
+        };
+    })();
+
+    var DefaultListView = (function(){
+        return {
+            linkSelector : ".text > a" ,
+            isValidLink : function( href ){
+                return href.indexOf( "detail" ) < 0 ;
+            } ,
+            insertStar : function ( elem , img ){
+                $( elem ).append( img );
+            } ,
+            insertSample : function () {}
+
+        } ;
+    })();
+
+    var ActressView = (function(){
+        return {
+            linkSelector : "td > a" ,
+            isValidLink : function( href ){
+                return href.indexOf( "product" ) < 0 ;
+            } , 
+            insertStar : function ( elem , img ){
+                img = $( img );
+                img.css( { "float" : "right" , "display" : "inline" , "margin" : 0 , "padding" : 0} );
+                $( elem ).append( img );
+            } ,
+            insertSample : function ( elem , sample) {
+                sample = $(sample);
+                $( elem ).append( sample );
+                sample.css( { display : "none" , "position" : "absolute" } );
+
+                $(elem).
+                    mouseover( function( event ){
+                        sample.css( { 
+                            display : "block" 
+//                            , top  : event.pageY  +"px" , 
+//                            left : event.pageX + "px" 
+                        } );
+                    }).
+                    mouseout( function(){
+                        sample.css( { display : "none" } );
+                    });
+
+            }
+        } ;
+    })();
 
     function setStarts( list ){
         list.each( function( i , elem ){
-            if( elem.href.indexOf("detail") < 0 ){ continue; }
+            if( currentView.isValidLink( elem.href ) ){ return; }
             $.get( elem.href , null , function( data ){
-                var ire = imgRegExp ;
-//                var s = Number( new Date() );
-//                var div = doc.createElement( "div" );
-//                div.innerHTML = getTag( data , "div" , "review-list" );
-                var div = getTag( data , "div" , "review-list" );
-                var img = div.match( ire )[0];
-//                console.log( div.innerHTML );
-//                console.log( Number( new Date() ) - s , img );
-                if( img.indexOf("http://p.dmm.co.jp/p/ms/review/") < 0 ){return;}
+                var sample = PlainHTMLUtil.getTag( data , "div" , "sample-video" ) ;
 
-                $( elem ).append( img );
+                //------------------------------------------
+                var ire = imgRegExp ;
+                var div = $( PlainHTMLUtil.getTag( data , "div" , "review-list" ) );
+
+                var img = $( "img" , div ).get(0);
+                if( img.src.indexOf("http://p.dmm.co.jp/p/ms/review/") < 0 ){return;}
+                img = $(img).parent().get(0);
+
+                currentView.insertStar   ( elem , img );
+                currentView.insertSample ( elem , sample ); 
+
                 return;
             });
         });
@@ -38,16 +102,15 @@
         setStarts( $( ".text > a" , t ) );
     }
 
-    function getTags(html, tagName, id){
-      var cls = "";
-      if(id){
-        cls = "[^>]*?id=\"" + id + "\"";
-      }
-      var reg = new RegExp("<" + tagName + cls + "(\\s|.)*?>([^<]*)</" + tagName + ">", "ig");
-      return html.match(reg);
+    function isActressView(){
+        if( location.href.indexOf( "actress.dmm.co.jp" ) >= 0 ){
+            return true;
+        }
+        return false ;
     }
 
-    function getTag(html, tagName, className){
-      var tags = getTags(html, tagName, className);
-      return (tags && tags.length) ? tags[0] : "";
-    }
+
+    var currentView = isActressView() ? ActressView : DefaultListView ; 
+    doc.addEventListener('DOMNodeInserted', handleInserted, false);
+    setStarts( $( currentView.linkSelector ) );
+
